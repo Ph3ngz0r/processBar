4f67bd4d6f1c14ab00740a970601c4e6ae79bcfe
r0am3r <r0am3r@protonmail.com>
2020-07-29 16:17:56 +0800

    update

--- processBar.go
+++ processBar.go
@@ -3,48 +3,65 @@ package processBar
 import "fmt"
 
 type Bar struct {
-    percent int64  //百分比
-    cur     int64  //当前进度位置
-    total   int64  //总进度
+    percent int  //百分比
+    cur     int  //当前进度位置
+    total   int  //总进度
     rate    string //进度条
     graph   string //显示符号
+    accuracy    int //一个 graph 代表 accuracy 个 percent，最小为1(默认值)，最大100
 }
 
 // 创建进度条
-func (b *Bar) NewBar (start, total int64) {
+func (b *Bar) NewBar (start, total int) {
     b.cur = start
     b.total =total
     if b.graph == "" {
         b.graph = "█"
     }
+    if b.accuracy == 0 {
+        b.accuracy = 1
+    }
     b.percent = b.getPercent()
-    for i := 0; i < int(b.percent); i += 2 {
+    for i := 0; i < int(b.percent); i += b.accuracy {
         b.rate += b.graph
     }
 }
 
 // 自定义进度条图案
-func (b *Bar) NewBarWithCustomGraph(start, total int64, graph string) {
+func (b *Bar) NewBarWithCustomGraph(start, total int, graph string) {
     b.graph = graph
     b.NewBar(start, total)
 }
+// 自定义进度条精度
+func (b *Bar) NewBarWithCustomAccuracy(start, total, acc int) {
+    b.accuracy = acc % 11
+    b.NewBar(start, total)
+}
+// 自定义进度条图案和精度
+func (b *Bar) NewBarWithCustomGraphAndAccuracy(start, total, acc int, graph string) {
+    b.graph = graph
+    b.NewBarWithCustomAccuracy(start, total, acc)
+}
 
 // 进度条百分比
-func (b *Bar) getPercent() int64 {
-    return int64(float32(b.cur) / float32(b.total) * 100)
+func (b *Bar) getPercent() int {
+    return int(float32(b.cur) / float32(b.total) * 100)
 }
 
 // 播放进度
-func (b *Bar) Play (cur int64) {
+func (b *Bar) Play (cur int) {
     b.cur = cur
     last := b.percent
     b.percent = b.getPercent()
-    if last != b.percent && b.percent % 2 == 0 {
+    if last != b.percent && b.percent % b.accuracy == 0 {
         b.rate += b.graph
+        fmt.Printf("\r%4d/%d %3d%% [%-1s]", b.cur, b.total, b.percent, b.rate)
     }
-    fmt.Printf("\r[%-50s]%3d%%  %8d/%d", b.rate, b.percent, b.cur, b.total)
 }
 
 func (b *Bar) Finish () {
+    b.graph = ""
+    b.accuracy = 0
+    b.rate =""
     fmt.Println()
 }
